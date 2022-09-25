3932c89265ec94efec359ef1adc1989c22ecec36
DenzCoder <100281304+denzcoder@users.noreply.github.com>
2022-09-25 13:25:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+<script>
+        ScrollReveal({
+            reset: true,
+            distance: '60px',
+            duration: 2500,
+            delay: 400
+        });
+
+        ScrollReveal().reveal('.title, .info-title', { delay: 500, origin: 'left'});
+        ScrollReveal().reveal('.first-sec .image', { delay: 600, origin: 'bottom'});
+        ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right'});
+        ScrollReveal().reveal('.second-sec .image, .third-sec .image', { delay: 500, origin: 'top'});
+        ScrollReveal().reveal('.media-icons img', { delay: 500, origin: 'top'});
+        ScrollReveal().reveal('.fill', { delay: 100, origin: 'left'});
+        ScrollReveal().reveal('.fill-one', { delay: 600, origin: 'right', interval: 200});
+    </script>
